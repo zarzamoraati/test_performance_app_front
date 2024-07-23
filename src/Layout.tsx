5fc50cb1b6915169c4ba5f0b84421d63c1c12066
zarzamoraati <zarzamoraati@gmail.com>
2024-07-23 13:06:30 -0600

    nav 1

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,21 @@ export const Layout = () => {
         <a className='navbar-brand'></a>
         <button className='navbar-toggler'>
             <span className='navbar-toggler-icon'>
-                
+                <ul className='nabvar-nav '>
+                    <li className='navbar-item'>
+                        <a className='navbar-link'
+                        href="/">
+                            Home
+                        </a>
+                    </li>
+                    <li className='navbar-item'>
+                        <a className='navbar-link'
+                        href="/about">
+                                About                            
+                        </a>
+                    </li>
+                    
+                </ul>
             </span>
         </button>
         <div className='navbar-collapse collapse' id="example">
