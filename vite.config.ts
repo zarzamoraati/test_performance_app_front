6836bf41553d298e86f1d62e5c40518359527563
zarzamoraati <zarzamoraati@gmail.com>
2024-07-23 13:01:16 -0600

    base fixed 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://zarzamoraati.github.io/test_performance_app_front"
+  base:"https://zarzamoraati.github.io/test_performance_app_front/"
 })
